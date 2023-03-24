9eaaadd738a0a259a0675a36389cdd4221c1f7dd
Your Pavel <p9l-real-chel@mail.ru>
2023-03-24 22:39:53 +0500

    WOrk

---
+++ Work41/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 10);
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+        Console.Write($"{item} ");
+    Console.WriteLine();
+}
+
+int Zero(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] < 0)
+            count++;
+    return count;
+}
+
+
+Console.WriteLine("Введите целое число:");
+int arrayLenth = (Convert.ToInt32(Console.ReadLine()));
+int [] array = new int [arrayLenth];
+FillArray(array);
+PrintArray(array);
+;
+Console.WriteLine($"Чисел меньше нуля в массиве: {Zero (array)}");
---
+++ workNega/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача НЕГАФИБОНАЧЧИ. Задайте число. Составьте массив чисел НегаФибоначчи, 
+// в том числе для отрицательных индексов.
+// *Пример:*
+// - для k = 9 массив будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+
+
+
+
+int[] Fibon(int count)
+{
+    int[] res = new int[count];
+    res[0] = 0;
+    res[1] = 1;
+    for (int i = 2; i < res.Length; i++)
+    {
+        res[i] = res[i - 2] + res[i - 1];
+    }
+    return res;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+        Console.Write($"{item} ");
+    Console.WriteLine();
+}
+
+void PrintRevFib(int [] array ,int num)
+{
+    for (int i = num -1; i > 0; i--)
+    {
+        if (i % 2 == 0)
+            array[i] *= (-1);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+System.Console.WriteLine("Введите номер числа фиббоначи: ");
+int num = Convert.ToInt32(Console.ReadLine());
+PrintRevFib(Fibon(num), num);
+PrintArray(Fibon(num));
